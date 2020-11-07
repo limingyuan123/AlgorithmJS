12cbab7e7c34ba464a04a792af2ca4badee72bd1
limingyuan123 <1484156313@qq.com>
2020-11-07 16:35:36 +0800

    11.7

--- jz/打印1到最大的n位数.js
+++ jz/打印1到最大的n位数.js
@@ -1,9 +1,13 @@
 var n = 3;
 function consoleN(n){
+    if(n<=0){return null}
     var re = 1;
     for(let i=0;i<n;i++){
         re*=10;
     }
+    for(let i=1;i<re;i++){
+        console.log(i);
+    }
 }
 var res = consoleN(n);
-console.log(res);
+// console.log(res);
