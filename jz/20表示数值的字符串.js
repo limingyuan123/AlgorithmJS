0028e6279d20310cb050bf690211ccd9513bf78e
limingyuan123 <1484156313@qq.com>
2020-11-12 00:14:04 +0800

    11.12

--- jz/20表示数值的字符串.js
+++ jz/20表示数值的字符串.js
@@ -1,21 +1,63 @@
-var str = '3.1416';
+var str = '123.45e+6';
 //A[.[B]][e|EC]  /  .B[e|EC]
 function matchNumber(str){
-    //匹配+-
-    if(str[0] === "+"||str[0] === "-"){
-        if(str[1] ==="+"||str[1] === "-"){
-            return false;
-        }else{
-            str = str.substring(1);
-            for(let i=0;i<str.length;i++){
-                if(str[i] === "."){
-                    str[i+1]
-                }
-            }
-        }
-    }else{//匹配字符串前无+-
-
+    var match = false;
+    if(str == null){
+        return false;
     }
+
+    //匹配+-
+    if((str[0] === "+"||str[0] === "-")&&(str[1] ==="+"||str[1] === "-")){
+            return false;
+    }
+
+    //匹配整数
+    var flag = 0;
+    for(let i=0;i<str.length;i++){
+        if(str[i] === '.'){
+            flag = i;
+            break;
+        }
+    }
+    if(flag>0){
+    match = matchUnsignInter(0,str.substring(flag));
+    str = str.substring(flag+1);
+    }
+
+    //匹配小数
+    flag = 0;
+    for(let i=0;i<str.length;i++){
+        if(str[i] === 'e'||str[i] === 'E'){
+            flag = i;
+            break;
+        }
+    }
+    if(flag>0){
+        match = matchInter(0,str.substring(flag));
+        str = str.substring(flag+1);
+    }
+
+    //匹配指数
+}
+function matchInter(str){
+    for(let i = 0;i < str.length;i++){
+        if(str[i] - '0'>=0&&str[i] - '0'<=9){
+            continue;
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
+function matchUnsignInter(str){
+    for(let i = 0;i < str.length;i++){
+        if(str[i] - '0'>=0&&str[i] - '0'<=9){
+            continue;
+        }else{
+            return false;
+        }
+    }
+    return true;
 }
 var res = matchNumber(str);
 console.log(res);
