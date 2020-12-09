4ff979748ece5bb6a6e5fbdb94f5cf2734783d7e
limingyuan123 <1484156313@qq.com>
2020-12-09 13:05:39 +0800

    优化15

--- jz/15二进制中1的个数.js
+++ jz/15二进制中1的个数.js
@@ -15,3 +15,19 @@ function find1(input){
 }
 var res = find1(input);
 console.log(res);
+
+//优化版
+/**
+ * 思路：把一个整数减去1，再和原整数做与运算，会把该整数最右边一个1变成0.那么一个整数的二进制有多少个1，就可以进行多少次这样的操作
+ * @param {*} n 
+ */
+function NumberOf1(n)
+{
+    // write code here
+    var count = 0;
+    while(n!=0){
+        count++;
+        n = n&(n-1);
+    }
+    return count;
+}
