356c6393c6794cb80de8fb1a351cde3d441d4924
limingyuan123 <1484156313@qq.com>
2020-12-13 22:36:35 +0800

    12.13

--- test.js
+++ test.js
@@ -1,26 +1,36 @@
-var array = [[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]];
-var target = 16;
-function Find(target, array)
+function RandomListNode(x){
+    this.label = x;
+    this.next = null;
+    this.random = null;
+}
+function Clone(pHead)
 {
     // write code here
-    if(array == null||target == null||array.length == 0){
-        return false;
+    if(pHead == null){
+        return null;
     }
-    let raw = array.length;
-    let col = array[0].length;
-    if(raw == null||col == null){
-        return false;
+    var hash = new Map();
+    //首次遍历，将random存起来
+    var node = pHead.next;
+    var i=0;
+    hash.set(i++,pHead.random);
+    var cHead = new RandomListNode(pHead.val);
+    var pre = cHead;
+    while(node!=null){
+        var newNode = new RandomListNode(node.val);
+        pre.next = newNode;
+        pre = newNode;
+        hash.set(i++,node.random);
+        node = node.next;
+    }    
+    //连接random
+    node = cHead;
+    for(let item in hash.values()){
+        node.random = item;
+        node = node.next;
     }
-    for(let i=0,j=col-1;i<raw,j>=0;){
-        if(target === array[i][j]){
-            return true;
-        }else if(target<array[i][j]){
-            j--;
-        }else{
-            i++;
-        }
-    }
-    return false;
+    return cHead;
 }
-let res = Find(target,array);
-console.log(res);
+module.exports = {
+    Clone : Clone
+};
